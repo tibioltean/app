ddd1148f714ff2f09e65ccfd31a90644f199a4d4
Tiberiu Oltean <olteantiberiu@gmail.com>
2015-08-27 16:46:55 +0300

    versiunea 1.0 test db funcions
    select, insert, update, delete si calendar

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -11,6 +11,39 @@ class Home extends CI_Controller
 		$this->load->view('home/home_view');
 		$this->load->view('home/inc/footer_view');	
 	}
-}
 
-?>
+	// Acesasta parte se face un model
+	public function test_db()
+	{
+		//$q = $this->db->get('user');
+		//*** Prima varianta cu active records cu get_where cu array
+		//$q = $this->db->get_where('user',['user_id' => 3]);
+		
+		//$this->db->where(['user_id' => 2]);
+		//$q = $this->db->get('user');
+  		//print_r($q->result());
+
+		$this->db->select('user_id,login');
+		$this->db->order_by('user_id DESC');
+		$q = $this->db->get('user');
+		print_r($q->result());
+
+        
+		$data = array(
+        'login' => 'ATD33',
+        'password' => 'SoparolaCEMo',       
+		);
+		
+		$this->db->where('user_id', 3);
+ 		$this->db->update('user', $data);
+
+	 	//$this->db->insert('user', $data);		
+	}
+
+	public function agenda(){
+		  $this->load->library('calendar');
+          echo $this->calendar->generate();
+
+	}
+
+}?>
